9d24acdd71ebec7609015422a919872768195d8b
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-03-10 22:27:33 +0000

    installed mongoose for mongodb connection

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app = express();
 
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("connected to mongoDb");
+}
+
 app.listen(5000, () => {
+  main();
   console.log("server running...");
 });
