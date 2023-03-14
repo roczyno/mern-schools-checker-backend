146992e992a317e6a214b7793954f86e1bf5103d
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-03-14 22:51:30 +0000

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import uccRouter from "./routes/ucc.route.js";
+import knustRouter from "./routes/knust.route.js";
+import ugRouter from "./routes/ug.route.js";
+import uewRouter from "./routes/uew.route.js";
 import cors from "cors";
 
 dotenv.config();
@@ -13,9 +16,16 @@ async function main() {
 }
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://schools-checker.onrender.com"],
+  })
+);
 
 app.use("/api/ucc", uccRouter);
+app.use("/api/knust", knustRouter);
+app.use("/api/ug", ugRouter);
+app.use("/api/uew", uewRouter);
 app.listen(5000, () => {
   main();
   console.log("server running...");
